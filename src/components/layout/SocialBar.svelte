7ace80cf5e4ee3a3e09a658cd0cbdc996fba9b53
Alexander Liu <humans@alexanderliu.com>
2022-09-19 11:31:32 -0700

    feat: add sticky social links

---
+++ src/components/layout/SocialBar.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Discord from "./Discord.svelte";
+  import Instagram from "./Instagram.svelte";
+  import Mail from "./Mail.svelte";
+
+  let element: HTMLDivElement;
+  let expand = true;
+
+  onMount(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry) {
+        expand = entry.isIntersecting;
+      }
+    });
+    observer.observe(element);
+  });
+</script>
+
+<div bind:this={element} />
+<div class="socials">
+  <a href="https://discord.gg/kSpR9uv">
+    <div class="icon">
+      <Discord />
+    </div>
+    {#if expand}
+      Discord
+    {/if}
+  </a>
+  <a href="https://instagram.com/cyberuci/">
+    <div class="icon">
+      <Instagram />
+    </div>
+    {#if expand}
+      Instagram
+    {/if}
+  </a>
+  <a
+    href="https://uci.us18.list-manage.com/subscribe?u=0029dcacaf8ca6a3b88c61750&id=701d18798e"
+  >
+    <div class="icon">
+      <Mail />
+    </div>
+    {#if expand}
+      Mailing List
+    {/if}
+  </a>
+</div>
+
+<style>
+  .socials {
+    position: sticky;
+    bottom: calc(16px + env(safe-area-inset-bottom));
+    display: flex;
+    gap: 8px;
+    overflow-y: scroll;
+    white-space: nowrap;
+  }
+
+  a {
+    color: var(--gray12);
+    background-color: var(--gray3);
+    padding: 8px 16px;
+    border-radius: 1000px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
+
+  .icon {
+    width: 16px;
+    height: 16px;
+  }
+</style>
