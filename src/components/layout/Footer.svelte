f7c8532537f8d905549d48ec9665aaa2e2d39584
Alexander Liu <humans@alexanderliu.com>
2022-09-18 23:20:35 -0700

    refactor: move common elements to layout file

---
+++ src/components/layout/Footer.svelte
@@ -0,0 +1,17 @@
+<footer>
+  <span class="copyright">Copyright © 2022 Cyber @ UCI</span>
+</footer>
+
+<style lang="scss">
+  footer {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    padding: 16px 0;
+  }
+
+  .copyright {
+    color: var(--gray11);
+    font-size: 0.875rem;
+  }
+</style>
